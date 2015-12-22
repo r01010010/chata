d4f00ff8701e27b25ec450043ebc4f131008c81e
r01010010 <r01010010@MacBook-MacBook-Pro-de-Wilhelmina.local>
2015-12-22 18:04:32 +0100

    User distiction by socket.id implemented

--- index.js
+++ index.js
@@ -3,14 +3,28 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = {};
+
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/chat.html');
 });
 
 io.on('connection', function(socket){
+
+    var user = {
+        id: socket.id,
+        nickname: 'unknown'
+    };
+
+    users[socket.id] = user;
+
+    io.to(socket.id).emit('user', user);
+
     socket.on('chat message', function(msg){
         io.emit('chat message', msg);
     });
+
+    console.log(users);
 });
 
 http.listen(3000, function(){
